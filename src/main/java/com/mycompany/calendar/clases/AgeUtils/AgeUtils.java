97587afb00f32938293661954e154b677bb726b1
Marl8n <midp515@gmail.com>
2020-09-21 14:57:34 -0600

    Se creo la funcionalidad de calcular edad y el metodo que regresa informacion como string en Person

---
+++ src/main/java/com/mycompany/calendar/clases/AgeUtils/AgeUtils.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calendar.clases.AgeUtils;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+/**
+ *
+ * @author marl8n
+ */
+public class AgeUtils {
+    public static Function<Integer, String> addZeros = x -> x < 10 ? "0" + x : String.valueOf(x);
+    
+    public static TriFunction<Integer, Integer, Integer, LocalDate> parseDate =
+            (day, month, year) -> LocalDate.parse(
+                    year + "-" + addZeros.apply(month) + "-" + addZeros.apply(day)
+            );
+    
+    public static TriFunction<Integer, Integer, Integer, Integer> calculateAge =
+            (day, month, year) -> Period.between(
+                    parseDate.apply(day, month, year), LocalDate.now()
+            ).getYears();
+    
+    //inferencia de datos
+@FunctionalInterface
+public interface TriFunction<T, U, V, R> {
+    R apply(T t, U u, V v);
+}
+}
+
---
+++ src/main/java/com/mycompany/calendar/clases/Person.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calendar.clases;
+
+import com.mycompany.calendar.clases.AgeUtils.AgeUtils;
+import com.mycompany.calendar.clases.AgeUtils.AgeUtils.TriFunction;
+import static com.mycompany.calendar.clases.AgeUtils.AgeUtils.calculateAge;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author marl8n
+ */
+public class Person implements TriFunction{
+    
+    private String dpi;
+    private String name;
+    private Date birthDate;
+    private Integer age;
+    
+    public Person (){
+        
+    }
+    
+    public Person (String dpi, String name, Date birthDate)
+    {
+        this.dpi = dpi;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.calcAge();
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getAge() {
+        return age;
+    }    
+    
+    
+    private void calcAge(){
+        Calendar c = Calendar.getInstance();
+        c.setTime(birthDate);
+        Integer year = c.get(Calendar.YEAR);
+        Integer month = c.get(Calendar.MONTH);
+        Integer day = c.get(Calendar.DAY_OF_MONTH);
+        
+        this.age = calculateAge.apply(day, month, year);
+        
+    }
+    
+    
+    public String getInfo(){
+        return this.name + ", " + this.age;
+    }
+
+    @Override
+    public Object apply(Object t, Object u, Object v) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
